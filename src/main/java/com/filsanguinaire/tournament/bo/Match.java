62e7252aa51f2af8a02013d12581341c50aab247
Laeti-Challant <l.challant@gmail.com>
2026-04-03 19:07:45 +0200

    feat(entities) : add Match entity and MatchStatus enum

---
+++ src/main/java/com/filsanguinaire/tournament/bo/Match.java
@@ -0,0 +1,46 @@
+package com.filsanguinaire.tournament.bo;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+
+@Entity
+public class Match {
+	@Id
+	@GeneratedValue(strategy =GenerationType.IDENTITY )
+	private Long id;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false,  length = 10)
+	private MatchStatus status = MatchStatus.PENDING;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "round_id", nullable= false)
+	private Round round;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "coach1_id")
+	private Coach coach1;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "coach2_id")
+	private Coach coach2;
+}
