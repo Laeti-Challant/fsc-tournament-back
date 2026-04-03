87ad04c53785c7e48959cf36a2d80cc45fc50c01
Laeti-Challant <l.challant@gmail.com>
2026-04-03 20:54:12 +0200

    feat(entities) : add rules set entities

---
+++ src/main/java/com/filsanguinaire/tournament/bo/AllowedInducement.java
@@ -0,0 +1,41 @@
+package com.filsanguinaire.tournament.bo;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+
+@Entity
+public class AllowedInducement {
+	@Id
+	@GeneratedValue(strategy =GenerationType.IDENTITY )
+	private Long id;
+	
+	@Column(nullable = false, length = 100)
+	private String name;
+	
+	@Column(nullable = false)
+	private short minQty;
+	
+	@Column(nullable = false)
+	private short maxQty;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "rules_id", nullable = false)
+	private TournamentRules rules;
+}
