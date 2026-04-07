4df8f138adfb7e2d99cb03417f1a909888b0a525
Laeti-Challant <l.challant@gmail.com>
2026-04-07 21:57:42 +0200

    feat(authentication) : add UserService with interface and necessary DTOs

---
+++ src/main/java/com/filsanguinaire/tournament/dto/user/UserUpdatePseudoDTO.java
@@ -0,0 +1,13 @@
+package com.filsanguinaire.tournament.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserUpdatePseudoDTO {
+
+	private String pseudo;
+}
