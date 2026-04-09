2207389da47b7dc91ad82e303120b30a4c50d1e5
Laeti-Challant <l.challant@gmail.com>
2026-04-09 16:55:16 +0200

    feat(event and ruleset) : add custom queries to repositories event and TournamentRules

--- src/main/java/com/filsanguinaire/tournament/dal/TournamentRulesRepository.java
+++ src/main/java/com/filsanguinaire/tournament/dal/TournamentRulesRepository.java
@@ -1,9 +1,14 @@
 package com.filsanguinaire.tournament.dal;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.filsanguinaire.tournament.bo.TournamentRules;
 
 public interface TournamentRulesRepository extends JpaRepository<TournamentRules, Long> {
 
+	Optional<TournamentRules> findByEventId(Long eventId);
+	
+	boolean existsByEventId(Long eventId);
 }
