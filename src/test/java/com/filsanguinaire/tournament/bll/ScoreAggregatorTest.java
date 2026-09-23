2bf1c55e5c6340430afb49662a414bd6a5d93427
Laeti-Challant <l.challant@gmail.com>
2026-09-23 12:57:57 +0200

    test(ranking): handle several coaches in aggregation tests

--- src/test/java/com/filsanguinaire/tournament/bll/ScoreAggregatorTest.java
+++ src/test/java/com/filsanguinaire/tournament/bll/ScoreAggregatorTest.java
@@ -15,10 +15,15 @@ import com.filsanguinaire.tournament.dto.ranking.ScoreDTO;
 
 public class ScoreAggregatorTest {
 
-	private List<CoachResult> list;
+	private List<CoachResult> results;
 	
 	@BeforeEach
 	void setUp() {
+		Coach coach1 = new Coach();
+		Coach coach2 = new Coach();
+		coach1.setId(1L);
+		coach2.setId(2L);
+		
 		CoachResult coachResult1 = new CoachResult();
 		coachResult1.setId(1L);
 		coachResult1.setResult(MatchResult.WIN);
@@ -27,10 +32,8 @@ public class ScoreAggregatorTest {
 		coachResult1.setObjectives(2);
 		coachResult1.setBonusObjective(true);
 		coachResult1.setPasses(2);
-		coachResult1.setFoulActions(2);
-		Coach coach = new Coach();
-		coach.setId(1L);
-		coachResult1.setCoach(coach);
+		coachResult1.setFoulActions(2);		
+		coachResult1.setCoach(coach1);
 		
 		CoachResult coachResult2 = new CoachResult();
 		coachResult2.setId(2L);
@@ -41,25 +44,41 @@ public class ScoreAggregatorTest {
 		coachResult2.setBonusObjective(false);
 		coachResult2.setPasses(1);
 		coachResult2.setFoulActions(3);
-		coachResult2.setCoach(coach);
+		coachResult2.setCoach(coach1);
 		
-		list = new ArrayList<CoachResult>();
-		list.add(coachResult1);
-		list.add(coachResult2);
+		CoachResult coachResult3 = new CoachResult();
+		coachResult3.setId(3L);
+		coachResult3.setResult(MatchResult.DRAW);
+		coachResult3.setTouchdowns(0);
+		coachResult3.setCasualties(0);
+		coachResult3.setObjectives(1);
+		coachResult3.setBonusObjective(false);
+		coachResult3.setPasses(2);
+		coachResult3.setFoulActions(1);
+		coachResult3.setCoach(coach2);
+		
+		results = new ArrayList<CoachResult>();
+		results.add(coachResult1);
+		results.add(coachResult2);
+		results.add(coachResult3);
 	}
 	
 	@Test
 	void shouldReturnOneForNumberofWins() {
 		// Arrange
-		ScoreAggregator aggregator = new ScoreAggregator();
-		
+		ScoreAggregator aggregator = new ScoreAggregator();		
 		
 		// Act
-		List<ScoreDTO> scoreList = aggregator.aggregate(list);
+		List<ScoreDTO> scoreList = aggregator.aggregate(results);
 		
 		// Assert
-		assertEquals(1, scoreList.size());
-		assertEquals(1,  scoreList.getFirst().getNumberOfWins());
+		assertEquals(2, scoreList.size());
+		assertEquals(1, findByCoachId(scoreList, 1L).getNumberOfWins());
 		
+		
+	}
+	
+	private ScoreDTO findByCoachId(List<ScoreDTO> results, Long coachId) {
+		return results.stream().filter(s -> s.getCoachId().equals(coachId)).findFirst().orElseThrow();
 	}
 }
