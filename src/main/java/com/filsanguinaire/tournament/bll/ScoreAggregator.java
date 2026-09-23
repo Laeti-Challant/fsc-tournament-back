7dd265f677c0c2ed2f6db8d773d2aef0ccf61d11
Laeti-Challant <l.challant@gmail.com>
2026-09-23 12:55:26 +0200

    refactor(ranking): simplify score aggregation with computeIfAbsent

--- src/main/java/com/filsanguinaire/tournament/bll/ScoreAggregator.java
+++ src/main/java/com/filsanguinaire/tournament/bll/ScoreAggregator.java
@@ -10,38 +10,19 @@ import com.filsanguinaire.tournament.bo.MatchResult;
 import com.filsanguinaire.tournament.dto.ranking.ScoreDTO;
 
 public class ScoreAggregator {
-	public List<ScoreDTO> aggregate(List<CoachResult> list) {
-		Map<Long, ScoreDTO> map = new HashMap<>();
-		
-		for (CoachResult cr : list) {
-			 long id = cr.getCoach().getId();
-			 
-			 if (!map.containsKey(id)) {
-				 
-				 ScoreDTO score = ScoreDTO.builder()
-						 .coachId(id)
-						 .numberOfWins(cr.getResult() == MatchResult.WIN ? 1 : 0)
-						 
-						 .build();
-				 map.put(id, score);				
-			 } else {
-				 
-				 ScoreDTO oldScore = map.get(id);
-				 int wins = oldScore.getNumberOfWins();
-				 				 
-				 int winsToAdd = cr.getResult() == MatchResult.WIN ? 1 : 0;
-				 
-				 
-				 ScoreDTO newScore = ScoreDTO.builder()
-						 .coachId(id)
-						 .numberOfWins(wins + winsToAdd)						 
-						 .build();
-				 
-				 map.put(id, newScore);
-			 }
-		 }
-		
-		List<ScoreDTO> scores = new ArrayList<>(map.values());
-		return scores;
+	public List<ScoreDTO> aggregate(List<CoachResult> results) {
+		Map<Long, ScoreDTO> scoresByCoachId = new HashMap<>();
+
+		for (CoachResult cr : results) {
+			long id = cr.getCoach().getId();
+
+			int win = cr.getResult() == MatchResult.WIN ? 1 : 0;
+			
+			ScoreDTO score = scoresByCoachId.computeIfAbsent(id, k -> ScoreDTO.builder().coachId(k).build());
+		 
+			score.setNumberOfWins(score.getNumberOfWins() + win);
+		}
+
+		return new ArrayList<>(scoresByCoachId.values());
 	}
 }
